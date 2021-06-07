6699d2001880a21ac213cf0d1695675f2b345d9a
lorena-1204 <mlrc1204@gmail.com>
2021-06-07 13:02:39 -0500

    ej

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Contacto from './components/contacto';
+import Inicio from './components/inicio';
+import Nosotros from './components/nosotros';
+
+/* to ->  para que vaya al link.
+   Switch -> Contenido dinamico.
+   Path -> se declara la ruta. 
+   Route ->llamar a los componentes.
+-> Se declara de lo más especifico a lo más general o se trabaja con 
+   exact -> para indicar q routa va primero
+   NavLink -> Reemplasa a link y llama una clase activa */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className ="container">
+        <div className ="btn-group">
+          <Link to ="/" className="btn btn-dark"> 
+          Inicio
+          </Link>
+
+          <Link to="/nosotros" className="">
+           Nosotros
+          </Link>
+
+          <Link to="/contacto" className="btn btn-dark">
+            Contacto
+          </Link>
+        </div>
+       <h1> hi ... </h1>
+       <hr/>
+
+       <Switch>
+       
+        <Route path="/contacto">
+          <Contacto />
+        </Route>
+
+        <Route path="/nosotros">
+          <Nosotros />
+        </Route>
+
+        <Route path="/">
+            <Inicio />
+          
+        </Route>
+         
+       </Switch>
+
+     </div>
+    </Router>
+    
   );
 }
 
