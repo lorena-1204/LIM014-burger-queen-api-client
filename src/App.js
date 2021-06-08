4257b49217f14c8c5ce88bc48ec9fbb1c20fce25
lorena-1204 <mlrc1204@gmail.com>
2021-06-07 20:35:48 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Contacto from './components/contacto';
-import Inicio from './components/inicio';
-import Nosotros from './components/nosotros';
 
-/* to ->  para que vaya al link.
-   Switch -> Contenido dinamico.
-   Path -> se declara la ruta. 
-   Route ->llamar a los componentes.
--> Se declara de lo más especifico a lo más general o se trabaja con 
-   exact -> para indicar q routa va primero
-   NavLink -> Reemplasa a link y llama una clase activa */
+import AppRouter from './routers/AppRouter';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <div>
-          <Link to ="/" >
-            Inicio
-          </Link>
-          <Link to="/nosotros" className="">
-            Nosotros
-          </Link>
-          <Link to="/contacto" >
-            Contacto
-          </Link>
-        </div>
-         <h1> hi... </h1>
-         <hr />
-
-        <Switch>
-        <Route path="/" exact>
-            <Inicio />
-        </Route>
-        <Route path="/contacto">
-          <Contacto />
-        </Route>
-        <Route path="/nosotros">
-          <Nosotros />
-        </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+       <AppRouter/> 
+    </div>
   );
 }
 
