a4f253a616835278f77b4071c11a4a02897bbbc6
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-06-10 23:25:50 -0500

    Categories

--- src/routers/CategoriesRouter.jsx
+++ src/routers/CategoriesRouter.jsx
@@ -6,6 +6,8 @@ import {
     Redirect,
     useRouteMatch,
 } from "react-router-dom";
+import Menu from "../pages/administrador/menu";
+import Trabajadores from "../pages/administrador/trabajadores";
 import CategoriesPage from "../pages/CategoriesPage";
 import Historial from "../pages/mesero/historial";
 import Home from "../pages/mesero/home";
@@ -40,12 +42,15 @@ export default function CategoriesRouter() {
             <Switch>
                 <PrivateRoute exact path="/categories" component={CategoriesPage} />
                 <PrivateRoute path="/categories/mesero" component={MeseroRouter} />
+                <PrivateRoute path="/categories/administrador" component={AdministradorRouter} />
 
                 <Route exact path="/categories/cocina">
                     <h1>Puesto: Cocinero</h1>
                 </Route>
             
-
+                <Route exact path="/categories/administrador">
+                    <h1>Puesto: Administrador</h1>
+                </Route>
                 <Route path="*">
                     <Redirect to="/404" />
                 </Route>
@@ -77,6 +82,32 @@ function MeseroRouter() {
                   
                 <Route exact path={`${url}/historial`} component={Historial}/>
                  
+                <Route path="*">
+                    <Redirect to="/404" />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+function AdministradorRouter() {
+    const { url } = useRouteMatch();
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/categories/administrador">Menu</Link>
+                </li>
+                <li>
+                    <Link to="/categories/administrador/trabajadores">Trabajadores</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path={`${url}`} component={Menu}/>
+
+                <Route exact path={`${url}/trabajadores`} component={Trabajadores}/>
+                                  
                 <Route path="*">
                     <Redirect to="/404" />
                 </Route>
