e6ea927cff46c503b8c8320ba8b122493fb75470
lorena-1204 <mlrc1204@gmail.com>
2021-06-10 23:28:55 -0500

    cocina

--- src/routers/CategoriesRouter.jsx
+++ src/routers/CategoriesRouter.jsx
@@ -1,18 +1,11 @@
-import {
-    Link,
-    NavLink,
-    Switch,
-    Route,
-    Redirect,
-    useRouteMatch,
-} from "react-router-dom";
+import { Link,NavLink,Switch,Route,Redirect,useRouteMatch } from "react-router-dom";
 import CategoriesPage from "../pages/CategoriesPage";
-import Historial from "../pages/mesero/historial";
+import HistorialCocina from "../pages/cocina/historialCocina";
+import Proceso from "../pages/cocina/proceso";
+import HistorialMesero from "../pages/mesero/historialMesero";
 import Home from "../pages/mesero/home";
 import Status from "../pages/mesero/status";
 import PrivateRoute from "./PrivateRoute";
-
-
 
 export default function CategoriesRouter() {
     const { url } = useRouteMatch();
@@ -40,6 +33,7 @@ export default function CategoriesRouter() {
             <Switch>
                 <PrivateRoute exact path="/categories" component={CategoriesPage} />
                 <PrivateRoute path="/categories/mesero" component={MeseroRouter} />
+                <PrivateRoute path="/categories/cocina" component={CocinaRouter} />
 
                 <Route exact path="/categories/cocina">
                     <h1>Puesto: Cocinero</h1>
@@ -75,8 +69,34 @@ function MeseroRouter() {
 
                 <Route exact path={`${url}/status`} component={Status}/>
                   
-                <Route exact path={`${url}/historial`} component={Historial}/>
+                <Route exact path={`${url}/historial`} component={HistorialMesero}/>
                  
+                <Route path="*">
+                    <Redirect to="/404" />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+function CocinaRouter() {
+    const { url } = useRouteMatch();
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/categories/cocina">Proceso</Link>
+                </li>
+                <li>
+                    <Link to="/categories/cocina/historial">Historial</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path={`${url}`} component={Proceso} />
+
+                <Route exact path={`${url}/historial`} component={HistorialCocina} />
+
                 <Route path="*">
                     <Redirect to="/404" />
                 </Route>
