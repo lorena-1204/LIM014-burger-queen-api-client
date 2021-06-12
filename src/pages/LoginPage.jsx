f9d4102869acb3d8d2b37bc2851b36e99c683703
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-06-11 20:26:49 -0500

    sass structure && login styles

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -15,16 +15,24 @@ export default function LoginPage() {
     }
 
     return (
-        <section>
-            <h1>Burguer Queen</h1>
-            <form>
-                <input type="email" placeholder='Usuario'></input> 
-                <input type="password" placeholder='************'></input>
-                <button onClick={handleLogin}>
-                    Iniciar Sesión
-                </button>
-            </form>
-         
+        <section className="page-login">
+
+            <div className="login-form">
+                <h1 className="login-form__title">Burguer Queen</h1>
+                <form className="login-form__form" autocomplete="off">
+                    <div className="login-form__form__input-group">
+                        <input className="input-default"  type="email" placeholder='Usuario'></input>
+                        <input className="input-default"  type="password" placeholder='Password'></input>
+                    </div>
+
+                    <button className="button-default" onClick={handleLogin}>
+                        Iniciar Sesión
+                    </button>
+
+                </form>
+            </div>
+
+
         </section>
     )
 }
