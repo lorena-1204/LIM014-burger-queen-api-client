bb80432460b1c56faaf5b2162b62226b0786dee0
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-06-16 11:07:20 -0500

    hom,e mesero sass

--- src/pages/mesero/home.jsx
+++ src/pages/mesero/home.jsx
@@ -1,8 +1,36 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1> <br></br>
-            <div>🌊ola o hola 👋≧◉ᴥ◉≦ </div>
+        <div className="page-home">
+            {/* columna izquierda */}
+            <div className="left-column"> 
+                <div className="client-name__input-area">
+                    <div className="login-form__form__input-group">
+                    <input className="input-default"  type="name" placeholder='Nombre del cliente'></input>
+                    <input className="input-default"  type="number" placeholder='Numero de mesa'></input>
+                    </div>
+
+                    <button className="button-default">Guardar</button>
+                </div>
+                    <div className="button-group">
+                         <button className="button-group__food">Desayuno</button>
+                         <button className="button-group__food">Almuerzo y Cena</button>
+                    </div>
+            </div>
+
+             {/* columna derecha*/}
+             <div className="right-column"> 
+                <div className="client-name__input-area">
+                    <div className="login-form__form__input-group">
+                    <input className="input-default"  type="name" placeholder='Nombre del cliente'></input>
+                    <input className="input-default"  type="number" placeholder='Numero de mesa'></input>
+                    </div>
+
+                    <button className="button-default">Guardar</button>
+                </div>
+            </div>
+           
+            
+            
         </div>
     )
 }
