7bae008d7b2241e5d9f651137cd0e8928b0124cf
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-06-24 12:33:32 -0500

    new code

--- src/pages/mesero/home.jsx
+++ src/pages/mesero/home.jsx
@@ -1,5 +1,6 @@
 import { useState , useEffect } from "react"
 import { urlApi } from "../../controller/URL"
+import { getProducts } from "../../services/products";
 
 export default function Home() {
     
@@ -7,26 +8,11 @@ export default function Home() {
     
   const [products, setProducts] = useState()
 
-    // definir funcion se usa Async porque incluye cosas asincronas
-    const fetchApi = async (url) => {
-        //Respuesta de la funcion fetch a la url(dataProducts) y es un await  porque se debe esperar y retorna una promesa
-        const response = await fetch(url, {
-            headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MGMzNGEzMWI2NjZlZTE3OThkMzFlOGQiLCJlbWFpbCI6ImFkbWluQGxvY2FsaG9zdCIsInJvbGVzIjp7ImFkbWluIjp0cnVlfSwiaWF0IjoxNjIzNTU0NzU4LCJleHAiOjk5OTk5OTk5OTk5fQ.zGMhPbJxmlZUvznOr76NqBnI2DKx0l4612qdET0-66w'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-        }) /*Se alamacena la respuesta de la url */
-        // console.log(response.status)
-
-        //se procesa la respuesta(parsea), interpretandola como json
-        const responseJSON = await response.json()
-        return responseJSON
-        // console.log(responseJSON.products)
-     }
+    // definir funcion se usa Async porque incluye cosas asincrona
 
     // useEffect, es un hook de react que permite encargarnos del ciclo de vida de react,se le va decir  no trnga dependecias al estar el array vacio y se ejecuta al iniciar la aplicacion por primera y unica vez
      useEffect(() =>{
-     fetchApi(`${urlApi}/products`
+     getProducts(`${urlApi}/products`
      ).then(res =>{
         setProducts(res.products)
      })
