a2a7a552ebc677b52b82ad88305bd5b717affc79
lorena-1204 <mlrc1204@gmail.com>
2021-06-30 00:00:32 -0500

    Login-token

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,42 +1,112 @@
-// import { useState, useEffect } from "react"
+import { useState } from "react";
+// import { useHistory, useLocation } from "react-router-dom";
+import { postAuth } from "../services/auth";
 
-import { useHistory, useLocation } from 'react-router-dom';
-import { useAuth } from '../services/auth';
 
-export default function LoginPage() {
-    const history = useHistory(); /*nos permite navegar a otra página */
-    const location = useLocation(); /*permite obtener informacion de la url ,nos ayuda a almacenar el location en la ruta anterior */
-    // console.log (location);
-    // console.log(location.state?.from); ?si el state es nulo va dar undefined
-    /*➡trae la información del url⬆ u */
-    const previusObjectURL = location.state?.from;
+export default function LoginPage(/*{history}*/) {
 
-    const auth = useAuth();
-    const handleLogin = () => {
-        auth.login();
-        history.push(previusObjectURL || "/mesero")
-        /*⬆ se envia a la ruta anterior pero si es undefined entonces se envia a mesero- el estado de las rutas anteriores solo se esta guardando en las rutas privadas*/
+    // const auth = useAuth(); /*cONtexto */
+
+    // const { startSession, role } = auth
+
+    const [login, setLogin] = useState({
+        email: "",
+        password: ""
+    })
+
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+    const handleChange = (e) => {
+        console.log(`Actualizar la variable de estado`);
+        //    ( setEmail, setPassword)
+        setLogin
+            ({
+                ...login,
+                // email,
+                // password,
+                [e.target.name]: e.target.value
+            })
+    }
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        console.log("form", login);
+
+        postAuth(login.email, login.password)
+
+            .then(res => {
+                console.log("que eres", res);
+
+                if (res.statusText === 200) {
+
+                    console.log('postLogin', res.data.token);
+                    console.log('post', res);
+
+                    // startSession(res.data.token)
+                    // console.log("rol",role);
+
+                    // if (role) {
+                    //     console.log(role);
+                    //     history.push("/administrador")
+                    // }
+
+                    // else if (role === false){
+                    //     console.log(role);
+                    //     history.push("/rol")
+                    // }
+                }
+            })
+
+        try {
+            const { token } = await postAuth(login);
+
+            localStorage.setItem('token', token);
+        } catch (error) {
+            document.getElementById('error').textContent = error;
+        }
     }
 
     return (
         <section className="page-login">
 
             <div className="login-form">
-                <h1 className="login-form__title">Burguer Queen</h1>
-                <form className="login-form__form" autoComplete="off">
+
+                <h1 className="login-form__title"> Burguer Queen </h1>
+
+                <form
+                    className="login-form__form"
+                    autoComplete="off"
+                    onSubmit={handleLogin}>
+
                     <div className="login-form__form__input-group">
-                        <input className="input-default" type="email" placeholder='Usuario'></input>
-                        <input className="input-default" type="password" placeholder='Password'></input>
+                        <input className="input-default"
+                            type="email"
+                            name="email"
+                            placeholder='Usuario'
+                            onChange={handleChange}
+                            // value={email.email}
+                            value={login.email}
+                        />
+                        <input className="input-default"
+                            type="password"
+                            name="password"
+                            placeholder='Password'
+                            onChange={handleChange}
+                            // value={password.password}
+                            value={login.password}
+                        />
+
                     </div>
 
-                    <button className="button-default" onClick={handleLogin}>
+                    <button className="button-default"
+                        type="submit"
+                    // onClick={handleLogin}
+                    >
                         Iniciar Sesión
                     </button>
-
                 </form>
             </div>
-
-
 
         </section>
     )
