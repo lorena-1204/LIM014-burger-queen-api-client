4e2ecbbebbc64f193b8a4733073486496918e0d0
lorena-1204 <mlrc1204@gmail.com>
2021-06-29 23:59:44 -0500

    services

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,44 +1,28 @@
-import { useContext } from "react";
-import { AuthContext } from "./AuthProvider";
-import { urlApi } from "./URL";
-
-/*permite acceder al contexto
-par consumir el contexto se llama al useContext y se pasa el contexto que se quiere consumir en nuestro caso es AUthContextot
-- AuthProvider--> dotar a la aplicación como tal de un proveer, lo que se consume es el contexto
-- useContext --> es un hook, cuando lo utilizamos devuelve los valores que se trae del contexto( en nuestro caso el contextValue, el objet q trae el user, el login y logout )   */
-
-
-export function useAuth() {
-    const contextValue = useContext(AuthContext);
-    return contextValue;
-}
-
-//  export default function useAuth() {
-//    return useContext(AuthContext);
-//  }
+import { urlApi } from "../controller/url";
 
 export const postAuth = async (email, password) => {
     const response = await fetch(`${urlApi}/auth`, {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
+        body:
+            JSON.stringify
+                ({
+                    email,
+                    password
+                }),
+        headers: { 'Content-Type': 'application/json' }
     })
-    console.log(response.body);
+    console.log("cuerpo", response);
+    // console.log("que", response);
 
     const dataJSON = await response.json();
+
     return dataJSON
 
-        // if(response.status ===200){return dataJSON;
-        // }
-        // if (response.status === 400) {
-        //     return Promise.reject('El email y password son requeridos');
-        // }
-
-        .then((response) => {
-            if (response.status === 200) {
-                return dataJSON;
-            } if (response.status === 400) {
-                return Promise.reject('El email y password son requeridos');
-            }
-        })
+    //   .then((response) => {
+    //      if (response.status === 200) {
+    //          return dataJSON;
+    //      } if (response.status === 400) {
+    //          return Promise.reject('El email y password son requeridos');
+    //      }
+    //   })
 }
