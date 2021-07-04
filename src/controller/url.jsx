05d4b31f0aba7797b0ee2ad49dbd0bca45361b59
lorena-1204 <mlrc1204@gmail.com>
2021-07-04 12:34:12 -0500

    URL

--- src/controller/url.jsx
+++ src/controller/url.jsx
@@ -1,6 +1,8 @@
 // const urlApi = 'http://localhost:5000';
 
-const urlApi ='https://rociovalentin.herokuapp.com';
+// const urlApi ='https://rociovalentin.herokuapp.com';
+
+const urlApi = 'https://burger-queen-lrc.herokuapp.com';
 
 export {
     urlApi,
