6115c6f3f0b1c442d6dbac59fb80f53b08299f1e
lorena-1204 <mlrc1204@gmail.com>
2021-07-05 09:22:12 -0500

    users

--- src/services/users.js
+++ src/services/users.js
@@ -1,10 +1,26 @@
 import { urlApi } from "./url";
 
-export const getUsers = async () => {
+export const getUser = async () => {
     const response = await fetch(`${urlApi}/users`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
         Authorization: `Bearer ${localStorage.getItem('token')}`
+    })
+    console.log("CUERPO USER", response);
+
+    const dataJSON = await response.json();
+
+    return dataJSON
+}
+
+export const postUser = async (user) =>{
+    const response = await fetch(`${urlApi}/users`, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     })
     console.log("CUERPO USER", response);
 
