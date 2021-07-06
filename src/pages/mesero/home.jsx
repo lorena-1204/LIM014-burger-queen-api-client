309ebac16709e6f464f9eb54f107e7352c96d4f5
lorena-1204 <mlrc1204@gmail.com>
2021-07-05 19:52:01 -0500

    añadir

--- src/pages/mesero/home.jsx
+++ src/pages/mesero/home.jsx
@@ -20,19 +20,56 @@ export default function Home() {
 
 
     return (
-        <div>
-            <h1>Home</h1> 
-             <ul>
-                 {/*si no se lee productos se dice que se carga sino se muestra index=indice, para q no salga error se indica eñ key->es un valor unico por cada uno de los componentes que se renderiza */}
-                 {!products ? 'Cargando ...': 
-                    products.map((products,index) => {
-                    return <li key={index}>{products.name} 
-                    </li>
-                })
-                } 
-            </ul> 
+        <div className="page-home">
+            {/* columna izquierda */}
+            <div className="left-column">
+                <div className="client-name__input-area">
+                    <div className="login-form__form__input-group">
+                        <input className="input-default" type="name" placeholder='Nombre del cliente'></input>
+                        <input className="input-default" type="number" placeholder='Numero de mesa'></input>
+                    </div>
+                    <div><button className="button-default">Guardar</button></div>
 
-            <h2>🌊ola o hola 👋≧◉ᴥ◉≦ </h2>
+                </div>
+                <div className="select-area">
+
+                    <select className="select-area__select" id="select-category">
+                        <option value="none">--</option>
+                        <option> </option>
+                        <option> </option>
+                    </select>
+                </div>
+
+                <div className='page-home__products'>
+                    {!products ? 'Cargando ...' :
+                        products.map((products, index) => {
+                            return <div className='page-home__products__container' key={index}>{products.name}<ul>S/.{products.price}</ul><img className='page-home__products__img' src={products.image} alt="" />
+                            </div>
+
+                        })
+                    }
+
+                </div>
+            </div>
+
+            {/* columna derecha*/}
+            <div className="right-column__box-area">
+                <div className="right-column__box-area__text-area">
+                    <p>Cliente: </p>
+                    <p>Colaborador:</p>
+                    <p># orders:</p>
+                    <p>Mesa:</p>
+                    <div className="right-column__box-area__header">
+                        <p>#     Products    Precio </p>
+                    </div>
+                    <div>Borrar ezxtso</div>
+
+
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
